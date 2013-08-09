4ced2f3e60003095d70c34cb000659d2ed5e2cef
diegomvh <diegomvh@gmail.com>
2013-08-09 13:01:54 -0300

    Mejorando modelo de personas y domicilios

--- handlers/persona.js
+++ handlers/persona.js
@@ -1,13 +1,46 @@
 var PersonaModel = require('../models/persona');
 var logger = require('../utils/logger');
 
+// GET     /collection     Returns all records. You can give limit and offset parameters to paginate records.
+// GET     /collection/:id     Returns a single document
+// POST     /collection     Creates a new document
+// PUT     /collection/:id     Replaces an existing document
+// PATCH     /collection/:id     Updates a document
+// DELETE     /collection/:id     Removes an existing document
+// 
+// selector
+//     A Mongo query expression specifying which items to return.
+//     Examples:
+//         {"fruit":"apple","colour":"green"} (all items where 'fruit' is 'apple' and 'colour' is 'green')
+//         {"fruit":{"$exists":true}} (all items that have a 'fruit' property)
+//         Example Request:
+//             GET http://drowsy.example.com/fridge/crisper?selector={"fruit":{"$exists":true}}
+// 
+// sort
+//     An array of property-order pairs to sort on.
+//     Examples:
+//         ["fruit","DESC"] (sort by the 'fruit' property, in descending order)
+//         ["fruit","ASC"] (sort by the 'fruit' property, in ascending order)
+//         [["fruit","ASC"],["colour","DESC"]] (sort by the 'fruit' property first in ascending order and then by the 'colour' property in descending order)
+//         Example Request:
+//             GET http://drowsy.example.com/fridge/crisper?sort=[["fruit","ASC"],["colour","DESC"]]
+
 function getPersonas(req, res) {
     /*
-        TODO Validar
+        TODO Validar, injection
     */
-    var skip = Number(req.param("skip", 0)),
-        limit = Number(req.param("limit", 1000))
-    var query = PersonaModel.find().skip(skip).limit(limit);
+    var skip = Number(req.param("skip")) || 0,
+        limit = Number(req.param("limit")) || 1000,
+        selector = eval("(" + req.param("selector", "{}") + ")"),
+        sort = eval("(" + req.param("sort", "[]") + ")");
+    var query = PersonaModel.find(selector);
+    //if (sort)
+    //    query.sort(sort);
+    if (skip)
+        query.skip(skip);
+    if (limit)
+        query.limit(limit);
+    console.log(query)
     query.exec(function (err, personas) {
         if (!err) {
             res.send({
--- models/persona.js
+++ models/persona.js
@@ -1,22 +1,52 @@
 var mongoose = require('mongoose');
 
 var DomicilioSchema = new mongoose.Schema({
+    numero: Number,
+    calle: String,
     localidad: String,
+    provincia: String,
     descripcion: String,
-    provincia: String
+    location: [ Number ], // lat, lng
+    google_address: {
+        geometry: {
+            location: {
+                lat: { type: Number, required:true},
+                lng: { type: Number, required:true},
+            },
+            viewport: {
+                northeast: {
+                    lat: { type: Number, required:true},
+                    lng: { type: Number, required:true},
+                },
+                southwest: {
+                    lat: { type: Number, required:true},
+                    lng: { type: Number, required:true},
+                }
+            },
+            location_type: { type: String, required:true},
+        },
+        address_components: [{
+            short_name: { type: String, required:true},
+            types: [ String ],
+            long_name: String,
+        }],
+        partial_match: Boolean,
+        formatted_address: String,
+        types: [ String ],
+    }
 });
 
 var PersonaSchema = new mongoose.Schema({ 
     documento: { type: Number, required:true},
     tipo_documento: String,
+    nombres: String,
+    apellidos: String,
     nombre_completo: String,
     clase: Number,
     sexo: String,
     nacimiento: { type: Date },
-    nombres: String,
-    apellidos: String,
     ocupacion: String,
-    domicilio: { type: mongoose.Schema.Types.ObjectId, ref: 'Domicilio' },
+    domicilio_actual: { type: mongoose.Schema.Types.ObjectId, ref: 'Domicilio' },
     domicilios: [{
         fecha: { type: Date, default: Date.now },
         domicilio: { type: mongoose.Schema.Types.ObjectId, ref: 'Domicilio' }
