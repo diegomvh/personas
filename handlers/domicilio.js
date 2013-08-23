4ea182aec74141017f452f369f9db9d5e866ad75
diegomvh <diegomvh@gmail.com>
2013-08-23 11:34:18 -0300

    Nuevas funciones para la api

--- handlers/domicilio.js
+++ handlers/domicilio.js
@@ -1,10 +1,55 @@
 var DomicilioModel = require('../models/domicilio');
 var logger = require('../utils/logger');
 
+/*
+  radius = 10 / 111; // 10km; 1 arcdegree ~= 111km
+*/
+function getPaises(req, res) {
+    var query = DomicilioModel.find({"geoaddress.types": "country"}, "descripcion location");
+    query.exec(function (err, paises) {
+        if (!err) {
+            res.send({
+                offset: 0,
+                objects: paises,
+                total: paises.length
+            });
+        } else {
+            logger.log("error", err);
+        }
+    });
+}
+
+function getProvincias(req, res) {
+    var query = DomicilioModel.find({"geoaddress.types": "administrative_area_level_1"}, "descripcion location");
+    query.exec(function (err, provincias) {
+        if (!err) {
+            res.send({
+                offset: 0,
+                objects: provincias,
+                total: provincias.length
+            });
+        } else {
+            logger.log("error", err);
+        }
+    });
+}
+
+function getDepartamentos(req, res) {
+    var query = DomicilioModel.find({"geoaddress.types": "administrative_area_level_2"}, "descripcion location");
+    query.exec(function (err, departamentos) {
+        if (!err) {
+            res.send({
+                offset: 0,
+                objects: departamentos,
+                total: departamentos.length
+            });
+        } else {
+            logger.log("error", err);
+        }
+    });
+}
+
 function getLocalidades(req, res) {
-    /*
-        TODO Validar, injection
-    */
     var query = DomicilioModel.find({"geoaddress.types": "locality"}, "descripcion location");
     query.exec(function (err, localidades) {
         if (!err) {
@@ -20,6 +65,9 @@ function getLocalidades(req, res) {
 }
 
 function DomicilioHandler() {
+    this.getPaises = getPaises;
+    this.getProvincias = getProvincias;
+    this.getDepartamentos = getDepartamentos;
     this.getLocalidades = getLocalidades;
 };
 
--- routes.js
+++ routes.js
@@ -11,6 +11,9 @@ function name(req, res) {
 function setup(app, handlers) {
     app.get('/', index);
     app.get('/name', name);
+    app.get('/api/paises', handlers.domicilio.getPaises);
+    app.get('/api/provincias', handlers.domicilio.getProvincias);
+    app.get('/api/departamentos', handlers.domicilio.getDepartamentos);
     app.get('/api/localidades', handlers.domicilio.getLocalidades);
     app.get('/api/personas', handlers.persona.getPersonas);
     app.get('/api/personas/:id', handlers.persona.getPersonaById);
